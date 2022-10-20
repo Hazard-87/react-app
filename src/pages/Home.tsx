a1b434817da44b0142ab5c815a5eb067a692f186
Dmitriy <dpetryshkin@gmail.com>
2022-10-20 12:58:22 +0400

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HelloWorld } from '../components/HelloWorld/HelloWorld'
+
+const Home = () => {
+  return (
+    <div>
+      <HelloWorld title="Hello, World!!!" />
+    </div>
+  )
+}
+
+export default Home
